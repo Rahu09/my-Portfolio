9d9b3c9c45159bb3bb456d8d496efa118bbafb66
Rahul Tiwari <tiwarirahul0809@gmail.com>
2024-01-07 13:07:25 +0530

    experience section compleated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import About from "@/components/about";
+import Experience from "@/components/experience";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
+import SectionDevider from "@/components/section-devider";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDevider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
     </main>
   );
 }
